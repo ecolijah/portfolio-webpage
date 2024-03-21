07ab8a628b2a0348bed1e05673eee0270ef93044
ecolijah <ecolijah@gmail.com>
2024-03-20 22:48:28 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Controller from './components/Controller.vue';
+</script>
+
+<template>
+  <Controller />
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Controller.vue
@@ -0,0 +1,51 @@
+<script setup>
+</script>
+
+<template>
+    <div class="outer-container">
+        <div class="left-spacer"></div>
+        <div class="inner-container">
+            <p>hello jdao abdajbd abkj k vj   jjjj j jjj jjj jjj jjj jj jjj jdao jjj
+                jjj jjj jjj jjjj jjjj jjj jjj jjj jjj j
+            </p>
+        </div>
+        <div class="right-spacer"></div>
+    </div>
+
+
+</template>
+<style scoped>
+    .outer-container {
+        min-width: 100vw;
+        min-height: 200vh;
+        background-color: yellow;
+        display: flex;
+        flex-direction: row;
+        justify-content: center;
+
+    }
+
+    .left-spacer {
+        display: flex;
+        min-width: auto;
+        background-color: aqua;
+        
+    }
+
+    .right-spacer {
+        display: flex;
+        min-width: fill;
+        background-color: red;
+    }
+
+    .inner-container {
+        display: flex;
+        max-width: 1080px;
+        background-color: yellowgreen;
+
+    }
+
+    p {
+        font-size: 20pt;
+    }
+</style>
